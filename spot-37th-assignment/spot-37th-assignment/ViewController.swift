aee8ad8954da82f3c28d5284f07d86c5bfe2aacc
JaeSunEo <wotjs020708@gmail.com>
2025-10-30 22:09:24 +0900

    init: 프로젝트 생성 및 기본 설정

---
+++ spot-37th-assignment/spot-37th-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spot-37th-assignment
+//
+//  Created by 어재선 on 10/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
