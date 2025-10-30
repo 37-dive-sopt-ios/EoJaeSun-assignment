3a77fb09736c8e4bcec9687c9cdcce012c01d436
JaeSunEo <wotjs020708@gmail.com>
2025-10-31 01:06:47 +0900

    add: #1 - WelecomViewController 추가

---
+++ spot-37th-assignment/spot-37th-assignment/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  spot-37th-assignment
+//
+//  Created by 어재선 on 10/31/25.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
