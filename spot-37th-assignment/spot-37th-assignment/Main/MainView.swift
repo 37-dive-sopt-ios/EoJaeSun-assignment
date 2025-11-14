de952e6fb0038f08a2491736296c0434b149fe5e
JaeSunEo <wotjs020708@gmail.com>
2025-11-14 13:48:38 +0900

    feat: #3 - 그라데이션 추가

--- spot-37th-assignment/spot-37th-assignment/Main/MainView.swift
+++ spot-37th-assignment/spot-37th-assignment/Main/MainView.swift
@@ -12,21 +12,37 @@ import Then
 
 class MainView: BaseView {
 
+    private let gradientBackgroundView = UIView()
+    let gradientLayer = CAGradientLayer()
+    
+    
     private let navgationBar = MainNavigationbarView()
     private let serachbar = CustomSearchView()
     
+    
     override func setUI() {
+        addSubview(gradientBackgroundView)
+        
         [navgationBar,serachbar].forEach {
             addSubview($0)
+            
         }
+       
         
         
     }
     
     override func setStyle() {
-        
+        gradientLayer.colors = [
+            UIColor.baeminWhite.cgColor,
+            UIColor.baeminMint300.cgColor
+        ]
+        gradientBackgroundView.layer.insertSublayer(gradientLayer, at: 0)
     }
+    
     override func setLayout() {
+        
+        
         navgationBar.snp.makeConstraints {
             $0.leading.top.trailing.equalToSuperview().inset(16)
         }
@@ -35,6 +51,14 @@ class MainView: BaseView {
             $0.leading.trailing.equalToSuperview().inset(16)
         }
         
-        
+        gradientBackgroundView.snp.makeConstraints {
+            $0.top.equalTo(serachbar.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = gradientBackgroundView.bounds
     }
 }
