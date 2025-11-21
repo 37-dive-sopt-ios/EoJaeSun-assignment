18f6f6d3e3813fdb99d6fb86b0cef28ba9b617df
JaeSunEo <wotjs020708@gmail.com>
2025-11-21 21:07:24 +0900

    [fix/#5] 필요 없는 코드 삭제

--- spot-37th-assignment/spot-37th-assignment/Base/BaseViewController.swift
+++ spot-37th-assignment/spot-37th-assignment/Base/BaseViewController.swift
@@ -11,32 +11,11 @@ class BaseViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad 호출 - \(type(of: self))")
 
         setView()
         setAddTarget()
         setDelegate()
 
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("viewWillAppear 호출 - \(type(of: self))")
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("viewDidAppear 호출 - \(type(of: self))")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("viewWillDisappear 호출 - \(type(of: self))")
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("viewDidDisappear 호출 - \(type(of: self))")
     }
 
     func setView() {}
